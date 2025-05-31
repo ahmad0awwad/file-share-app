0c569cce9e69bd34b60443d801084217653e9246
ahmad0awwad <153103674+ahmad0awwad@users.noreply.github.com>
2025-05-31 15:18:49 +0300

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -11,5 +11,14 @@ const nextConfig: NextConfig = {
   output: "export", // Tells Next.js to use static export (next export)
   trailingSlash: true, // Optional: better compatibility with GitHub Pages
 };
+const repoName = "file-share-app";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
+};
+
 
 export default nextConfig;
