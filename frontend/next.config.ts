f28ec54bc12e806bc345804edf34f396d8b0d470
ahmad0awwad <153103674+ahmad0awwad@users.noreply.github.com>
2025-05-31 15:17:02 +0300

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Tells Next.js to use static export (next export)
+  trailingSlash: true, // Optional: better compatibility with GitHub Pages
 };
 
 export default nextConfig;
